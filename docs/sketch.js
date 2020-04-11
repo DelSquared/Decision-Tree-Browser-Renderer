9c1e5c49827d6451c344adb083cbc2def64eb2e3
DelSquared <37172200+DelSquared@users.noreply.github.com>
2020-04-11 20:41:36 +0200

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -4,7 +4,6 @@ let h = 1000;
 function Tree(node_text){
   this.text = node_text;
   this.subnodes = [];
-  this.textsize = 16;
   this.add_subnode = function (node_text){
     this.subnodes.push(new Tree(node_text))
   }
@@ -13,7 +12,7 @@ function Tree(node_text){
     stroke(0);
     rect(x-30, y-5, 60, 60);
     fill(0);
-    textSize(this.textsize);
+    textSize(16);
     text(this.text, x-25, y, 50, 50);
     if (this.subnodes.length > 0){
       line(x, y+55, x, y+65)
@@ -23,7 +22,7 @@ function Tree(node_text){
         this.subnodes[0].render(x, y+80)
       }
       else{
-        line(x - 200*this.subnodes.length + 200, y+65, x + 200*this.subnodes.length + 100, y+65)
+        line(x - 100*this.subnodes.length + 200, y+65, x + 100*this.subnodes.length, y+65)
         for (let i = 0; i < this.subnodes.length; i++){
           line(x - (i - 0.5*this.subnodes.length)*200, y+65,x - (i - 0.5*this.subnodes.length)*200, y+80);
           this.subnodes[i].render(x - (i - 0.5*this.subnodes.length)*200, y+80)
@@ -39,7 +38,7 @@ function setup() {
   createCanvas(w, h);
   fill(0);
   stroke(0);
-  t = new Tree('test'); //example tree that renders on startup
+  t = new Tree('test'); //example
   t.add_subnode('sub');
   t.add_subnode('sub');
   t.add_subnode('sub');
